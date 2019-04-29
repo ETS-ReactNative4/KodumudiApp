6bd5fefb43b330e160b850843fb2f3a2d0f66280
Shafeeq <idc.mohdshafeeq@gmail.com>
2019-04-29 14:59:48 +0530

    new Changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./css/index.css";
 import HomeHeader from "./components/HomeHeader";
 import Icons from "./components/HomeBlocks";
 import SimpleBottomNavigation from "./components/bottomNav";
-import ProfileCard from "./layouts/ProfileCard";
 import * as serviceWorker from "./serviceWorker";
 ReactDOM.render(<HomeHeader />, document.getElementById("HomeHeader"));
 ReactDOM.render(<Icons />, document.getElementById("HomeBlocks"));
@@ -12,7 +11,6 @@ ReactDOM.render(
   <SimpleBottomNavigation />,
   document.getElementById("BottomNav")
 );
-ReactDOM.render(<ProfileCard />, document.getElementById("ProfileCard"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
